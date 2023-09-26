e32a5992b67c87610f63805375318f10df859cd4
Rofogale <marcoaureliodgomes@gmail.com>
2023-09-25 23:02:55 -0300

    Subindo Aula6 e correção no trabalho

---
+++ aulas/Aula6/js/script.js
@@ -0,0 +1,27 @@
+// prompt("Eu som um prompt");
+
+// console.log("Mensagem de Console");
+
+// let nome = "Marco";
+
+// function obtemNome() {
+//   console.log(nome);
+// }
+
+// obtemNome();
+
+// const nome = prompt("Digite seu nome: ");
+// console.log(nome);
+
+const idade = 13;
+
+console.log(typeof idade);
+
+let num1 = prompt("Digite um numero: ");
+num1 = parseFloat(num1);
+console.log(num1);
+let num2 = parseFloat(prompt("Digite outro numero: "));
+console.log(num2);
+let soma = (num1 + num2) / 2;
+console.log(soma);
+alert("A média dos números é: " + soma);
